0e67b8a53bee91d25f8d1a24c2e534f40f3e3fd4
Laura Trapaga <trapaga9@gmail.com>
2023-03-12 18:46:27 -0400

    Add index.js code

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const db = require('./config/connection');
+const routes = require('./routes');
+
+//* Gets current folder when you run node from a specific folder in terminal
+const cwd = process.cwd();
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
+});
