fbbb5e3efd520d97695fd2773d5e699e3153dc92
Laura Trapaga <trapaga9@gmail.com>
2023-03-16 15:53:13 -0400

    Add connection.js code

---
+++ config/connection.js
@@ -0,0 +1,15 @@
+const { connect, connection } = require('mongoose');
+
+// Node will look for this environment variable and if it exists, it will use it
+// Otherwise, it will assume that you are running this application locally
+const connectionString =
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialmediaDB';
+
+// Wrap Mongoose around connection to MongoDB
+connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Export connection 
+module.exports = connection;
