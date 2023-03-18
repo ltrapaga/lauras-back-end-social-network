4d5592c0351eceed463b747a766418bbcd0287ae
Laura Trapaga <trapaga9@gmail.com>
2023-03-17 20:51:59 -0400

    Add createNewUser to userRoutes and userController
        Add createNewUser router to userRoutes
        Add createNewUser function with User.create method to userController to POST a new user's data
            Tested with Insomnia

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,9 +1,18 @@
 const router = require("express").Router();
 
-const { getUsers, getSingleUser } = require("../../controllers/userController");
+const {
+  getUsers,
+  getSingleUser,
+  createNewUser,
+} = require("../../controllers/userController");
 
-router.route("/").get(getUsers);
+router
+  .route("/")
+  .get(getUsers)
+  .post(createNewUser);
 
-router.route("/:userId").get(getSingleUser);
+router
+  .route("/:userId")
+  .get(getSingleUser);
 
 module.exports = router;
