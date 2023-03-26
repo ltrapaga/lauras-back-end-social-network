f3be895044c327b4e20eda875e296cf0e60e9e79
Laura Trapaga <trapaga9@gmail.com>
2023-03-26 15:21:33 -0400

    Add getSingleThought to routes and controllers     Add getSingleThought router to thoughtRoutes     Add getSingleThought function to thoughtController         Thought.findOne method to GET a single thought by ID     Tested with Insomnia

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -7,4 +7,14 @@ module.exports = {
       .then((thoughts) => res.json(thoughts))
       .catch((err) => res.status(500).json(err));
   },
+  // Get a single thought by ID
+  getSingleThought(req, res) {
+    Thought.findOne({ _id: req.params.thoughtId })
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: "No thought found with that ID" })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
 };
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -2,11 +2,17 @@ const router = require("express").Router();
 
 const {
   getThoughts,
+  getSingleThought
 } = require("../../controllers/thoughtController");
 
 // /api/thoughts
 router
   .route("/")
-  .get(getThoughts)
+  .get(getThoughts);
+
+// /api/thoughts/:thoughtId
+router
+  .route("/:thoughtId")
+  .get(getSingleThought);
 
 module.exports = router;
